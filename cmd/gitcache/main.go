a38938c88e5e94e9a26cea107c6aa046caae5730
Adam Eijdenberg <adam.eijdenberg@gmail.com>
2017-01-17 21:41:27 +1100

    Refactor so that it can be run as standalone command.

--- cmd/gitcache/main.go
+++ cmd/gitcache/main.go
@@ -20,8 +20,10 @@ package main
 
 import (
 	"archive/tar"
+	"compress/gzip"
 	"crypto/sha256"
 	"encoding/hex"
+	"errors"
 	"flag"
 	"io"
 	"log"
@@ -30,16 +32,11 @@ import (
 	"os"
 	"os/exec"
 	"path"
+	"path/filepath"
 	"strings"
 	"time"
 
 	homedir "github.com/mitchellh/go-homedir"
-)
-
-var (
-	cacheDir       string
-	webBind        string
-	listenProtocol string
 )
 
 func makeCommand(cmd string, args ...string) *exec.Cmd {
@@ -48,19 +45,34 @@ func makeCommand(cmd string, args ...string) *exec.Cmd {
 	return exec.Command(cmd, args...)
 }
 
-func normalizeTar(in io.ReadCloser, out http.ResponseWriter) {
-	tarIn := tar.NewReader(in)
+func fetchUpstream(gd, repo, branch string) error {
+	return makeCommand("git", "--git-dir", gd, "fetch", repo, "+"+branch+":"+branch).Run()
+}
+
+func sendDownstream(gd, commit, tree string, out io.Writer) error {
+	cmd := makeCommand("git", "--git-dir", gd, "archive", "--format", "tar", commit+":"+tree)
+	pipeTar, err := cmd.StdoutPipe()
+	if err != nil {
+		return err
+	}
+
+	err = cmd.Start()
+	if err != nil {
+		return err
+	}
+
+	tarIn := tar.NewReader(pipeTar)
 	tarOut := tar.NewWriter(out)
+	defer tarOut.Close()
 
 	for {
 		header, err := tarIn.Next()
 		if err != nil {
 			if err == io.EOF {
-				tarOut.Flush()
+				return cmd.Wait() // normal exit point
 			} else {
-				log.Println("Unexpected error reading tar:", err)
+				return err
 			}
-			return
 		}
 
 		// Reset modification time to constant value else we get non-deterministic
@@ -69,145 +81,147 @@ func normalizeTar(in io.ReadCloser, out http.ResponseWriter) {
 
 		err = tarOut.WriteHeader(header)
 		if err != nil {
-			log.Println("Error writing tar:", err)
-			return
+			return err
 		}
 
 		written, err := io.CopyN(tarOut, tarIn, header.Size)
 		if err != nil {
-			log.Println("Error copying tar data:", err)
-			return
+			return err
 
 		}
 
 		if written != header.Size {
-			log.Println("Error copying tar data:", written, header.Size)
-			return
+			return err
 		}
 	}
 }
 
-func handleFetch(w http.ResponseWriter, r *http.Request) {
-	repo := r.FormValue("repo")
+func getHeadCommit(gd, repo, branch string) (string, error) {
+	err := fetchUpstream(gd, repo, branch)
+	if err != nil {
+		return "", err
+	}
+
+	commitHex, err := makeCommand("git", "--git-dir", gd, "rev-parse", branch).Output()
+	if err != nil {
+		return "", err
+	}
+
+	return strings.TrimSpace(string(commitHex)), nil
+}
+
+// Return git workspace dir that is ready to go
+func preflightAndInit(repo, branch, format, cacheDir string) (string, error) {
 	if len(repo) == 0 {
-		http.Error(w, "Must specify repo", 400)
-		return
+		return "", errors.New("Must specify repo")
 	}
-	branch := r.FormValue("branch")
 	if len(branch) == 0 {
-		http.Error(w, "Must specify branch, even if you know the commit (we may need it to fetch)", 400)
-		return
+		return "", errors.New("Must specify branch, even if you know the commit (we may need it to fetch)")
 	}
-	commit := r.FormValue("commit") // optional
-	tree := r.FormValue("tree")     // optional
-	format := r.FormValue("format") // required
 	if len(format) == 0 {
-		http.Error(w, "Must specify format, e.g. tgz", 400)
-		return
+		return "", errors.New("Must specify format, e.g. tgz")
 	}
 
-	if format != "tar" {
-		http.Error(w, "Format must be tar for now", 400)
-		return
+	if format != "tar" && format != "tgz" {
+		return "", errors.New("Format must be tar or tgz for now")
 	}
 
-	// First, make sure workspace exists
+	// Make sure workspace exists
 	hash := sha256.Sum256([]byte(repo))
 	gd := path.Join(cacheDir, hex.EncodeToString(hash[:]))
 	_, err := os.Stat(gd)
-	if err != nil && os.IsNotExist(err) {
-		err = os.MkdirAll(gd, 0755)
-		if err == nil {
+	if err != nil {
+		if os.IsNotExist(err) {
+			err = os.MkdirAll(gd, 0755)
+			if err != nil {
+				return "", err
+			}
 			err = makeCommand("git", "--git-dir", gd, "init", "--bare").Run()
+			if err != nil {
+				return "", err
+			}
+		} else {
+			return "", err
 		}
 	}
+
+	return gd, nil
+}
+
+// If outputDir is "", write to w. Else write file to outpuDir, and name of file to w
+func fetchLatest(repo, branch, commit, tree, format, cacheDir string, outputDir string, ourOutput io.Writer) error {
+	gd, err := preflightAndInit(repo, branch, format, cacheDir)
 	if err != nil {
-		log.Print("Error creating git dir: ", gd, err)
-		http.Error(w, "Cannot create git dir", 500)
-		return
+		return err
 	}
 
-	// If no commit is specified, fetch latest and set.
 	haveFetched := false
+	// If no commit is specified, fetch latest and set.
 	if len(commit) == 0 {
-		err = makeCommand("git", "--git-dir", gd, "fetch", repo, "+"+branch+":"+branch).Run()
+		commit, err = getHeadCommit(gd, repo, branch)
 		if err != nil {
-			http.Error(w, "Error fetching from repo", 502)
-			return
+			return err
 		}
 		haveFetched = true
+	}
 
-		commitHex, err := makeCommand("git", "--git-dir", gd, "rev-parse", branch).Output()
+	var w io.Writer
+	if len(outputDir) == 0 {
+		w = ourOutput
+	} else {
+		fpath := filepath.Join(outputDir, commit+"."+format)
+		os.Stdout.Write([]byte(fpath))
+		f, err := os.Create(fpath)
 		if err != nil {
-			http.Error(w, "Error fetching latest commit from repo", 502)
-			return
+			return err
 		}
+		defer f.Close()
+		w = f
+	}
 
-		commit = strings.TrimSpace(string(commitHex))
+	if format == "tgz" {
+		gzipper := gzip.NewWriter(w)
+		defer gzipper.Close()
+
+		w = gzipper
 	}
 
 	// Optimistically try, will fail if we don't have the commit, but it's cheap to try
-	cmd := makeCommand("git", "--git-dir", gd, "archive", "--format", "tar", commit+":"+tree)
-	pipeTar, err := cmd.StdoutPipe()
-	if err != nil {
-		http.Error(w, "Error running archive", 502)
-		return
+	err = sendDownstream(gd, commit, tree, w)
+	if err == nil {
+		return nil
 	}
 
-	err = cmd.Start()
-	if err != nil {
-		http.Error(w, "Error running archive", 502)
-		return
-	}
-	normalizeTar(pipeTar, w)
-	err = cmd.Wait()
-
-	if err != nil && !haveFetched {
-		// If we haven't fetched already, try one more time
-		err = makeCommand("git", "--git-dir", gd, "fetch", repo, "+"+branch+":"+branch).Run()
-		if err != nil {
-			http.Error(w, "Error fetching from repo", 502)
-			return
-		}
-
-		haveFetched = true
-
-		cmd := makeCommand("git", "--git-dir", gd, "archive", "--format", "tar", commit+":"+tree)
-		pipeTar, err = cmd.StdoutPipe()
-		if err != nil {
-			http.Error(w, "Error running archive", 502)
-			return
-		}
-
-		err = cmd.Start()
-		if err != nil {
-			http.Error(w, "Error running archive", 502)
-			return
-		}
-		normalizeTar(pipeTar, w)
-		err = cmd.Wait()
+	if haveFetched {
+		return err
 	}
 
+	// If we haven't fetched already, try one more time
+	err = fetchUpstream(gd, repo, branch)
 	if err != nil {
-		// may be too late, but try to write error code
-		http.Error(w, "Error running archive", 502)
-		return
+		return err
+	}
+
+	return sendDownstream(gd, commit, tree, w)
+}
+
+func makeHandleFetch(cacheDir string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		err := fetchLatest(r.FormValue("repo"),
+			r.FormValue("branch"),
+			r.FormValue("commit"),
+			r.FormValue("tree"),
+			r.FormValue("format"),
+			cacheDir, "", w)
+		if err != nil {
+			log.Println("Error:", err.Error())
+			http.Error(w, err.Error(), 400)
+		}
 	}
 }
 
-func main() {
-	flag.StringVar(&cacheDir, "cachedir", "~/.gitcache", "Directory to use for caching. May get quite large")
-	flag.StringVar(&webBind, "webbind", ":9091", "Binding for webserver.")
-	flag.StringVar(&listenProtocol, "protocol", "tcp4", "Listen on tcp or tcp4")
-	flag.Parse()
-
-	var err error
-	cacheDir, err = homedir.Expand(cacheDir)
-	if err != nil {
-		log.Fatal("homedir.Expand: ", err)
-	}
-
-	http.HandleFunc("/fetch", handleFetch) // set router
+func runServer(listenProtocol, webBind, cacheDir string) error {
+	http.HandleFunc("/fetch", makeHandleFetch(cacheDir)) // set router
 
 	ln, err := net.Listen(listenProtocol, webBind) // explicit listener since we want ipv4 today
 	if err != nil {
@@ -229,8 +243,50 @@ func main() {
 
 	log.Print("Serving on " + webBind)
 
-	err = http.Serve(ln, nil)
+	return http.Serve(ln, nil)
+}
+
+func main() {
+	var (
+		cacheDir       string
+		webBind        string
+		listenProtocol string
+
+		repo   string
+		tree   string
+		branch string
+		commit string
+		format string
+
+		outDir string
+	)
+
+	flag.StringVar(&cacheDir, "cachedir", "~/.gitcache", "Directory to use for caching. May get quite large")
+	flag.StringVar(&webBind, "webbind", ":9091", "Binding for webserver.")
+	flag.StringVar(&listenProtocol, "protocol", "tcp4", "Listen on tcp or tcp4")
+
+	flag.StringVar(&repo, "repo", "", "Repository to fetch from, required or a server will start instead.")
+	flag.StringVar(&tree, "tree", "", "Tree to filter, by default get all")
+	flag.StringVar(&branch, "branch", "", "Required, branch containing commit")
+	flag.StringVar(&commit, "commit", "", "Optional - if not specified will always contact server")
+	flag.StringVar(&outDir, "outdir", ".", "Directory to write output.")
+	flag.StringVar(&format, "format", "tgz", "tar or tgz")
+
+	flag.Parse()
+
+	cacheDir, err := homedir.Expand(cacheDir)
 	if err != nil {
-		log.Fatal("Serve: ", err)
+		log.Fatal("homedir.Expand: ", err)
 	}
+
+	if len(repo) == 0 {
+		err = runServer(listenProtocol, webBind, cacheDir)
+	} else {
+		err = fetchLatest(repo, branch, commit, tree, format, cacheDir, outDir, os.Stdout)
+	}
+
+	if err != nil {
+		log.Fatal("Error: ", err)
+	}
+
 }
