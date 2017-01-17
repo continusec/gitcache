a99540bde17fa6ab33c550bf5c5b51292b97f928
Adam Eijdenberg <adam@continusec.com>
2017-01-17 16:25:16 +1100

    Adjust modification times to a constant value, otherwise git gives us non-deterministic output for the same commit (if dir tree is specified).

--- cmd/gitcache/main.go
+++ cmd/gitcache/main.go
@@ -19,9 +19,11 @@ limitations under the License.
 package main
 
 import (
+	"archive/tar"
 	"crypto/sha256"
 	"encoding/hex"
 	"flag"
+	"io"
 	"log"
 	"net"
 	"net/http"
@@ -29,18 +31,60 @@ import (
 	"os/exec"
 	"path"
 	"strings"
+	"time"
+
+	homedir "github.com/mitchellh/go-homedir"
 )
 
 var (
-	CacheDir       string
-	WebBind        string
-	ListenProtocol string
+	cacheDir       string
+	webBind        string
+	listenProtocol string
 )
 
 func makeCommand(cmd string, args ...string) *exec.Cmd {
 	log.Println(cmd, strings.Join(args, " "))
 
 	return exec.Command(cmd, args...)
+}
+
+func normalizeTar(in io.ReadCloser, out http.ResponseWriter) {
+	tarIn := tar.NewReader(in)
+	tarOut := tar.NewWriter(out)
+
+	for {
+		header, err := tarIn.Next()
+		if err != nil {
+			if err == io.EOF {
+				tarOut.Flush()
+			} else {
+				log.Println("Unexpected error reading tar:", err)
+			}
+			return
+		}
+
+		// Reset modification time to constant value else we get non-deterministic
+		// output from git
+		header.ModTime = time.Unix(0, 0)
+
+		err = tarOut.WriteHeader(header)
+		if err != nil {
+			log.Println("Error writing tar:", err)
+			return
+		}
+
+		written, err := io.CopyN(tarOut, tarIn, header.Size)
+		if err != nil {
+			log.Println("Error copying tar data:", err)
+			return
+
+		}
+
+		if written != header.Size {
+			log.Println("Error copying tar data:", written, header.Size)
+			return
+		}
+	}
 }
 
 func handleFetch(w http.ResponseWriter, r *http.Request) {
@@ -62,9 +106,14 @@ func handleFetch(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	if format != "tar" {
+		http.Error(w, "Format must be tar for now", 400)
+		return
+	}
+
 	// First, make sure workspace exists
 	hash := sha256.Sum256([]byte(repo))
-	gd := path.Join(CacheDir, hex.EncodeToString(hash[:]))
+	gd := path.Join(cacheDir, hex.EncodeToString(hash[:]))
 	_, err := os.Stat(gd)
 	if err != nil && os.IsNotExist(err) {
 		err = os.MkdirAll(gd, 0755)
@@ -99,8 +148,20 @@ func handleFetch(w http.ResponseWriter, r *http.Request) {
 
 	// Optimistically try, will fail if we don't have the commit, but it's cheap to try
 	cmd := makeCommand("git", "--git-dir", gd, "archive", "--format", "tar", commit+":"+tree)
-	cmd.Stdout = w
-	err = cmd.Run()
+	pipeTar, err := cmd.StdoutPipe()
+	if err != nil {
+		http.Error(w, "Error running archive", 502)
+		return
+	}
+
+	err = cmd.Start()
+	if err != nil {
+		http.Error(w, "Error running archive", 502)
+		return
+	}
+	normalizeTar(pipeTar, w)
+	err = cmd.Wait()
+
 	if err != nil && !haveFetched {
 		// If we haven't fetched already, try one more time
 		err = makeCommand("git", "--git-dir", gd, "fetch", repo, "+"+branch+":"+branch).Run()
@@ -111,9 +172,20 @@ func handleFetch(w http.ResponseWriter, r *http.Request) {
 
 		haveFetched = true
 
-		cmd := makeCommand("git", "--git-dir", gd, "archive", "--format", format, commit+":"+tree)
-		cmd.Stdout = w
-		err = cmd.Run()
+		cmd := makeCommand("git", "--git-dir", gd, "archive", "--format", "tar", commit+":"+tree)
+		pipeTar, err = cmd.StdoutPipe()
+		if err != nil {
+			http.Error(w, "Error running archive", 502)
+			return
+		}
+
+		err = cmd.Start()
+		if err != nil {
+			http.Error(w, "Error running archive", 502)
+			return
+		}
+		normalizeTar(pipeTar, w)
+		err = cmd.Wait()
 	}
 
 	if err != nil {
@@ -124,19 +196,38 @@ func handleFetch(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	flag.StringVar(&CacheDir, "cachedir", "/tmp/gitcache", "Directory to use for caching.")
-	flag.StringVar(&WebBind, "webbind", ":9091", "Binding for webserver.")
-	flag.StringVar(&ListenProtocol, "protocol", "tcp4", "Listen on tcp or tcp4")
+	flag.StringVar(&cacheDir, "cachedir", "~/.gitcache", "Directory to use for caching. May get quite large")
+	flag.StringVar(&webBind, "webbind", ":9091", "Binding for webserver.")
+	flag.StringVar(&listenProtocol, "protocol", "tcp4", "Listen on tcp or tcp4")
 	flag.Parse()
+
+	var err error
+	cacheDir, err = homedir.Expand(cacheDir)
+	if err != nil {
+		log.Fatal("homedir.Expand: ", err)
+	}
 
 	http.HandleFunc("/fetch", handleFetch) // set router
 
-	ln, err := net.Listen(ListenProtocol, WebBind) // explicit listener since we want ipv4 today
+	ln, err := net.Listen(listenProtocol, webBind) // explicit listener since we want ipv4 today
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
 
-	log.Print("Serving on ", WebBind)
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		log.Fatal("net.InterfaceAddrs: ", err)
+	}
+
+	for _, a := range addrs {
+		if ipnet, ok := a.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				log.Print("(optional) export GITCACHE=http://" + ipnet.IP.String() + webBind)
+			}
+		}
+	}
+
+	log.Print("Serving on " + webBind)
 
 	err = http.Serve(ln, nil)
 	if err != nil {
